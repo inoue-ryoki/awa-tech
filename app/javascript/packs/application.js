0205018a8a770dc9e16d7f49ff82e5f0742e6e1e
inoue-ryoki <mth.1021@icloud.com>
2022-06-13 09:23:50 +0900

    bootstrap導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 
 require("@rails/activestorage").start()
 require("channels")
+import "bootstrap"
+import "../stylesheets/application"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
