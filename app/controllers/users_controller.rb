b277ad75578a877601458269fabfb174db8826e6
inoue-ryoki <mth.1021@icloud.com>
2022-06-19 08:09:23 +0900

    CSS調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,9 @@ class UsersController < ApplicationController
 def show
   @user = User.find(params[:id])
 
-      @currentUserEntry=Entry.where(user_id: current_user.id)
+      @currentUserEntry=Entry.where(user_id: current_user&.id)
     @userEntry=Entry.where(user_id: @user.id)
-    if @user.id == current_user.id
+    if @user.id == current_user&.id
     else
       @currentUserEntry.each do |cu|
         @userEntry.each do |u|
