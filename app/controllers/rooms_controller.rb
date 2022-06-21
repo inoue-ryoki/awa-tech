e983676889ad902c1416ad9f80c27f4ce0f8be7d
inoue-ryoki <mth.1021@icloud.com>
2022-06-21 20:04:52 +0900

    unicornの導入

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,11 +1,25 @@
 class RoomsController < ApplicationController
 
    before_action :authenticate_user!
+
+
   def create
     @room = Room.create
     @entry1 = Entry.create(room_id: @room.id,user_id: current_user.id)
     @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
     redirect_to "/rooms/#{@room.id}"
+  end
+
+    def index
+
+    # ログインユーザー所属ルームID取得
+    current_entries = current_user.entries
+    my_room_id = []
+    current_entries.each do |entry|
+      my_room_id << entry.room.id
+    end
+    # 自分のroom_idでuser_idが自分じゃないのを取得
+    @another_entries = Entry.where(room_id: my_room_id).where.not(user_id: current_user.id)
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+
+
   def edit
   end
 
