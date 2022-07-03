fc888df31e313108ab9e8ee28f21e0d6191d135b
inoue-ryoki <mth.1021@icloud.com>
2022-07-03 21:35:27 +0900

    ユーザー結合テストコード実装

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,111 @@
+require 'rails_helper'
+
+RSpec.describe 'ユーザー新規登録', type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  context 'ユーザー新規登録ができるとき' do
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      # トップページに移動する
+        visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+       expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+       image_path = Rails.root.join('public/images/test_image.png')
+       attach_file('user[image]', image_path, make_visible: true)
+       fill_in 'Name', with: @user.name
+       fill_in 'Email', with: @user.email
+       fill_in 'Password', with: @user.password
+       fill_in 'Password confirmation', with: @user.password_confirmation
+       fill_in 'School name', with: @user.school_name
+       fill_in 'Address', with: @user.address
+       fill_in 'Telephone number', with: @user.telephone_number
+       fill_in 'Teacher name', with: @user.teacher_name
+       fill_in 'School content', with: @user.school_content
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+        expect{
+        find('input[name="commit"]').click
+         }.to change { User.count }.by(1)
+      # トップページへ遷移したことを確認する
+        expect(current_path).to eq(root_path)
+      # トップページにログアウトボタンがあることを確認する
+      expect(page).to have_content('ログアウト')
+      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+        expect(page).to have_no_content('新規登録')
+        expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ユーザー新規登録ができないとき' do
+it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+  # トップページに移動する
+  visit root_path
+  # トップページにサインアップページへ遷移するボタンがあることを確認する
+  expect(page).to have_content('新規登録')
+  # 新規登録ページへ移動する
+  visit new_user_registration_path
+  # ユーザー情報を入力する
+       fill_in 'Name', with: ''
+       fill_in 'Email', with: ''
+       fill_in 'Password', with: ''
+       fill_in 'Password confirmation', with: ''
+       fill_in 'School name', with: ''
+       fill_in 'Address', with: ''
+       fill_in 'Telephone number', with: ''
+       fill_in 'Teacher name', with: ''
+       fill_in 'School content', with: ''
+  # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+  expect{
+    find('input[name="commit"]').click
+  }.to change { User.count }.by(0)
+  # 新規登録ページへ戻されることを確認する
+  expect(current_path).to eq user_registration_path
+end
+  end
+end
+
+RSpec.describe 'ログイン', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインができるとき' do
+    it '保存されているユーザーの情報と合致すればログインができる' do
+    # トップページに移動する
+    visit root_path
+    # トップページにログインページへ遷移するボタンがあることを確認する
+    expect(page).to have_content('ログイン')
+    # ログインページへ遷移する
+    visit new_user_session_path
+    # 正しいユーザー情報を入力する
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    # ログインボタンを押す
+    find('input[name="commit"]').click
+    # トップページへ遷移することを確認する
+    expect(current_path).to eq(root_path)
+   # トップページにログアウトボタンがあることを確認する
+     expect(page).to have_content('ログアウト')
+    # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
+    expect(page).to have_no_content('新規登録')
+    expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ログインができないとき' do
+    it '保存されているユーザーの情報と合致しないとログインができない' do
+   # トップページに移動する
+    visit root_path
+    # トップページにログインページへ遷移するボタンがあることを確認する
+    expect(page).to have_content('ログイン')
+    # ログインページへ遷移する
+    visit new_user_session_path
+    # ユーザー情報を入力する
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: ''
+    # ログインボタンを押す
+    find('input[name="commit"]').click
+    # ログインページへ戻されることを確認する
+    expect(current_path).to eq(new_user_session_path)
+    end
+  end
+end
