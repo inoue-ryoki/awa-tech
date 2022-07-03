5d83170e5f9c78a1a3c6f975e72ba4122213435d
inoue-ryoki <mth.1021@icloud.com>
2022-07-03 16:04:38 +0900

    画像プレビュー機能実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,8 @@ require("channels")
 import "bootstrap"
 import "../stylesheets/application"
 require("../delete");
-
+require('../preview')
+require('../preview_user')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
---
+++ app/javascript/preview.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // 新規投稿ページのフォームを取得
+  const postForm = document.getElementById('post_image');
+  // プレビューを表示するためのスペースを取得
+  const previewList = document.getElementById('previews');
+  // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
+  if (!postForm) return null;
+  console.log("preview.jsが読み込まれました");
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="post[image]"]');
+
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', function (e) {
+    // 古いプレビューが存在する場合は削除
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    // 画像を表示するためのdiv要素を生成
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    // 表示する画像を生成
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    // 生成したHTMLの要素をブラウザに表示させる
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+
+  });
+
+
+});
