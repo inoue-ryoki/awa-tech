b1f1d7dbb1d4b2d938656417d78057926c06bb2b
inoue-ryoki <mth.1021@icloud.com>
2022-07-11 14:40:28 +0900

    日本時間に変更

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module AwaTech
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Tokyo'
 
 
 
