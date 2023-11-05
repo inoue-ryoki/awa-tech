f54b3c2dacae5f7f5b9581dda607c7fe8fa9ec8f
inoue-ryoki <mth.1021@icloud.com>
2023-11-05 13:13:42 +0900

    非同期にはなってない

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,18 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :set_post, only: [:create, :destroy]
   def create
     @post_favorite = Favorite.new(user_id: current_user.id, post_id: params[:post_id])
     @post_favorite.save
-    redirect_to root_path
   end
 
   def destroy
     @post_favorite = Favorite.find_by(user_id: current_user.id, post_id: params[:post_id])
     @post_favorite.destroy
-    redirect_to root_path
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
